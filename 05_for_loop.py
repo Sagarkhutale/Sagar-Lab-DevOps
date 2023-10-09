c8b0804a04be5be3bbcc3f1d10f5643bfedd7d13
sagar <sagar.khutale5@gmail.com>
2023-10-09 21:30:04 +0530

    New_Commit

--- 05_for_loop.py
+++ 05_for_loop.py
@@ -11,8 +11,17 @@ Session.set_missing_host_key_policy(paramiko.AutoAddPolicy)
 Session.connect(ip, username=username, password=password, port=22, look_for_keys=False,allow_agent=False)
 
 SSH_Access = Session.invoke_shell()
-SSH_Access.send(b'terminal len 0\n')
-SSH_Access.send(b'show version\n')
+SSH_Access.send (b'config t\n')
+
+for N in range (1,10):
+    SSH_Access.send('int loopback '+str(N) + '\n')
+    SSH_Access.send('ip address 1.1.1.' +str(N) + ' 255.255.255.255\n')
+
+time.sleep(3)        
+SSH_Access.send(b'do terminal len 0\n')
+SSH_Access.send(b'do show version\n')
+SSH_Access.send(b'do show ip int bri\n')
+
 time.sleep(2)
 output = SSH_Access.recv(65000)
 print(output.decode('utf-8'))
